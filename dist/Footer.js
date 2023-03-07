d55c059f71b77dddf7857de9c99f6a5ec1b82cc6
mazusu <qweghj1245@gmail.com>
2023-03-07 14:53:02 +0800

    feat(): test

--- dist/Footer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export default function Footer() {
-  return /*#__PURE__*/React.createElement("div", null, "footerfooter");
-}
-;
--- dist/Head.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export default function Head() {
-  return /*#__PURE__*/React.createElement("div", null, "headhead");
-}
-;
